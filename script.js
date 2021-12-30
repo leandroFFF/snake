f46735754537b256a5bbf74e5c9257ef0e2646b2
leandroFFF <leandrofrancagti@gmail.com>
2021-12-29 21:00:29 -0300

    definir movimentos da cobrinha

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*DECLARANDO AS VARIAVEIS GLOBAIS*/
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
@@ -6,11 +7,15 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
+
+/*FUNCAO QUE CRIA O PLANO DE FUNDO*/
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+/*FUNCAO QUE CRIA A COBRINHA*/
 function criarCobrinha(){
     for(i=0;i < snake.length; i++){
         context.fillStyle = "green";
@@ -18,5 +23,28 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+/*FUNCAO PARA INICIAR O JOGO*/
+function iniciarJogo(){
+    /*CHAMADA DAS FUNCOES*/
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);   /*Atualiza o jogo a cada 100 ms*/
